e52e4e90781f67fb8b5c5d85404d0531931f7751
Asterith888 <talk2logan8@gmail.com>
2026-03-19 11:24:03 -0400

    Update ResourceWorldMod.java

--- src/main/java/com/asterith/resourceworld/ResourceWorldMod.java
+++ src/main/java/com/asterith/resourceworld/ResourceWorldMod.java
@@ -1,12 +1,20 @@
 package com.asterith.resourceworld;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class ResourceWorldMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ResourceWorldCommands.register();
+
+        // Load your world metadata, config, etc.
         ResourceWorldManager.initialize();
+
+        // Register commands the Fabric way
+        CommandRegistrationCallback.EVENT.register(
+            (dispatcher, registryAccess, environment) ->
+                ResourceWorldCommands.register(dispatcher, registryAccess)
+        );
     }
 }
